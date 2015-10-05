d4efe27ed405c2d98684304823837908b6776ab7
William Judd <wjiv33@gmail.com>
2015-10-04 20:47:57 -0400

    Added Cocoapods

--- NFL Superbowl Picks/AppDelegate.swift
+++ NFL Superbowl Picks/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import Firebase
 //import AmazonS3RequestManager
 
 @UIApplicationMain
---
+++ Pods/Target Support Files/Pods-NFL Superbowl Picks/Pods-NFL Superbowl Picks-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_NFL_Superbowl_Picks : NSObject
+@end
+@implementation PodsDummy_Pods_NFL_Superbowl_Picks
+@end

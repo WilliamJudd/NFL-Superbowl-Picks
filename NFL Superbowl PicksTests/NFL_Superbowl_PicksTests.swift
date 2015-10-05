040e777a6f9c9c8f75097a2ec21777a9cc0c9480
William Judd <wjiv33@gmail.com>
2015-10-04 20:00:20 -0400

    Initial Commit

---
+++ NFL Superbowl PicksTests/NFL_Superbowl_PicksTests.swift
@@ -0,0 +1,36 @@
+//
+//  NFL_Superbowl_PicksTests.swift
+//  NFL Superbowl PicksTests
+//
+//  Created by William Judd on 10/4/15.
+//  Copyright © 2015 William Judd. All rights reserved.
+//
+
+import XCTest
+@testable import NFL_Superbowl_Picks
+
+class NFL_Superbowl_PicksTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
